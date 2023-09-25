6211541a835962ade3e0338ebc858a2bcc40dda7
xunyuzi <3154792617@qq.com>
2023-09-25 19:58:42 +0800

    顺序栈框架

---
+++ SequentialStack/SqStack.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdbool.h>
+#include<malloc.h>
+
+//！！基于顺序存储结构的栈！！
+//本程序完成了顺序栈数据结构及部分算法
+
+typedef struct SqStack {
+
+	int* pBase;//存储首地址，作为栈底
+	int* pTop;//存储下一个待存地址，作为栈顶
+	int StackSize;//栈的最大空间
+
+}Stack,*PStack;
+
+
+bool InitStack();
+bool ClearStack();
+
+
+
+int main() {
+
+
+	return 0;
+}
